b3cebf9cae98707f7e1c1c84380106c8e8f19fbe
Kenneth Christensen <1kennethac@gmail.com>
2025-03-25 21:35:00 -0600

    Ollama compatible setup with environment variables for model configuration

--- chapter3/python/main.py
+++ chapter3/python/main.py
@@ -1,16 +1,25 @@
 import os
 import openai
 
+base_url = os.environ.get("OPENAI_BASE_URL")
+api_key = os.environ.get("OPENAI_API_KEY") or "ollama"
+model_name = os.environ.get("MODEL_NAME") or "llama3.2" if api_key == "ollama" else "gpt-3.5-turbo"
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-print(openai.api_key)
-
+client = openai.OpenAI(
+    base_url = base_url,
+    api_key=api_key,
+)
 
 def chat_with_gpt(
-    model, user_message, top_p=1, frequency_penalty=0, presence_penalty=0,temperature=0
+    model,
+    user_message,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0,
+    temperature=0
 ):
     try:
-        response = openai.ChatCompletion.create(
+        response = client.chat.completions.create(
             model=model,
             messages=[
                 {
@@ -30,9 +39,14 @@ def chat_with_gpt(
     
 # Example usage
 response = chat_with_gpt(
-    "gpt-3.5-turbo",
+    model_name,
     "Suggest a good name for a customer support chatbot working for a holiday company",
     top_p=0.9,
     frequency_penalty=-0.5,
     presence_penalty=0.6,
 )
+
+if isinstance(response, str):
+    print(response)
+else:
+    print(response.choices[0].message.content)
